14cf5d5277ae337dc1995b57f29b821ea5c21f6d
saksham345 <kumarsaksham601@gmail.com>
2022-12-14 22:06:55 +0530

    add and remove favourite movies

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {data} from '../data';
-import Navbar from './Navbar';
+import Navbar from './navbar';
 import MovieCard from './MovieCard';
 import { addMovies } from '../actions';
 
