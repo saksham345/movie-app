ca3cf8f32c2b68a7b1bae68ac762279c4c62c8f0
saksham345 <kumarsaksham601@gmail.com>
2022-12-14 22:02:05 +0530

    add and remove favourite movies

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,53 @@
-import React from "react";
+import React from 'react';
 import {data} from '../data';
-import Navbar  from "./navbar";
-import MovieCard from "./MovieCard";
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
+import { addMovies } from '../actions';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className='main'>
-        <div className='tabs'>
-          <div className='tab'>Movies</div>
-          <div className='tab'>Favourites</div>
+class App extends React.Component {
+
+  componentDidMount(){
+
+    this.props.store.subscribe( () => {
+      this.forceUpdate();
+      console.log(this.props.store.getState())
+    })
+    this.props.store.dispatch(addMovies(data))
+    
+  }
+
+  
+
+  isFavourite = (movie) =>{
+    let favourite = this.props.store.getState().favourites;
+    let index = favourite.indexOf(movie);
+    if(index === -1){
+      return false;
+    }
+    return true;
+  }
+  
+  render(){
+
+    return (
+      <div className="App">  
+        <Navbar />
+        <div className='main'>
+          <div className='tabs'>
+            <div className='tab'>Movies</div>
+            <div className='tab'>Favourites</div>
+          </div>
+          <div className='list'>
+            {this.props.store.getState().list.map( (movie , index) => {
+              return <MovieCard isFavourite={this.isFavourite(movie)} movie={movie} key={index} store={this.props.store} />
+            })}
         </div>
-        <div className='list'>
-          {data.map( (movie) => {
-            return <MovieCard movie={movie} />
-          })}
-       </div>
-     </div>
-    </div>
-  );
+      </div>
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,25 +1,41 @@
 import React from 'react';
+import { addFovourite,removeFavourite } from '../actions';
 
 class MovieCard extends React.Component {
+
+    handleFavouriteClick = () =>{
+        let movie = this.props.movie
+        this.props.store.dispatch(addFovourite(movie));
+    }
+
+    handleUnFavouriteClick = () =>{
+        let movie = this.props.movie
+        this.props.store.dispatch(removeFavourite(movie));
+    }
+
     render(){
-        const {movie} = this.props
+        const {movie,isFavourite} = this.props;
+
         return (
             <div className='movie-card'>
                 <div className='left'>
-                    <img src={movie.Poster} alt='movie-poster'/>
+                    <img src={movie.Poster} />
                 </div>
                 <div className='right'>
                     <div className='title'>{movie.Title}</div>
                     <div className='plot'>{movie.Plot}</div>
                     <div className='footer'>
                         <div className='rating'>{movie.imdbRating}</div>
-                        <button className='favourite-btn'>Favourites</button>
+                        {isFavourite?
+                            <button className='unfavourite-btn' onClick={this.handleUnFavouriteClick}>Unfavourite</button>:
+                            <button className='favourite-btn' onClick={this.handleFavouriteClick}>Favourite</button>
+                        }
                     </div>
                 </div>
             </div>
           );
     }
-
+  
 }
 
 export default MovieCard;
